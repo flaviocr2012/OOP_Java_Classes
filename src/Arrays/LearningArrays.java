8a3afad3936fe3aff1355a813f08821e80a2b37a
flavio <flaviocr2012@gmail.com>
2020-09-16 15:00:06 -0300

    Arrays

---
+++ src/Arrays/LearningArrays.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class LearningArrays {
+
+    public static void main(String[] args) {
+
+        int[] studentMathScore = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            studentMathScore[i] = i;
+
+            System.out.println(studentMathScore[i]);
+        }
+
+    }
+
+}
